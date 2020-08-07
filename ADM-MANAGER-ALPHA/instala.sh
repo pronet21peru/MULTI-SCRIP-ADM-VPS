15d810216866f3fa6124d6d6065e94c66ba78639
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-08-06 22:33:16 -0400

    Update Script

--- ADM-MANAGER-ALPHA/instala.sh
+++ ADM-MANAGER-ALPHA/instala.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-function_verify () {
-echo "verify" > $(echo -e $(echo 2f62696e2f766572696679737973|sed 's/../\\x&/g;s/$/ /'))
-echo " " > $(echo -e $(echo 2f7573722f62696e2f61646d2d756c74696d617465|sed 's/../\\x&/g;s/$/ /'))
-}
-
 fun_bar () {
 comando[0]="$1"
 comando[1]="$2"
@@ -34,11 +29,6 @@ instalar_fun () {
 cd /etc/adm-lite && bash cabecalho --instalar
 }
 
-versao_script () {
-v1=$(curl -sSL "https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-ALPHA/versaoatt")
-echo "$v1" > /etc/adm-lite/versao_script
-}
-
 elimined_fun () {
 text=$(source trans -b pt:${id} "Instalando")
 echo -e "${cor[2]} Update"
@@ -57,40 +47,61 @@ echo -e "${cor[2]} $text Figlet"
 fun_bar 'apt-get install lynx' 'apt-get install curl'
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1
-echo -e "${cor[1]}=================================== "
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 }
 
 valid_fun () {
+echo -e "${cor[2]}$(source trans -b pt:${id} "Opa!, Chave Valida! Instalando...")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 [[ -d /etc/adm-lite ]] && rm -rf /etc/adm-lite
 mkdir /etc/adm-lite
 cd /etc/adm-lite
 echo "cd /etc/adm-lite && bash ./menu" > /bin/menu && chmod +x /bin/menu
 echo "cd /etc/adm-lite && bash ./menu" > /bin/adm && chmod +x /bin/adm
-echo "cd /etc/adm-lite && bash ./menu" > /bin/h && chmod +x /bin/h
+_contador="1"
+while read arq_adm; do
 cd /etc/adm-lite
-touch /etc/adm-lite/index.html
-wget -i $HOME/lista -o /dev/null
-versao_script
-echo -e "${cor[3]}$(source trans -b pt:${id} "Ahora se instalarán las dependencias")"
-echo -e "${cor[1]}=================================== "
+wget $arq_adm -o /dev/null
+_contador=$(($_contador + 1))
+done < $HOME/lista
+echo -e "${cor[3]}$(source trans -b pt:${id} "Agora Sera Instalado As Dependencias")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 cd /etc/adm-lite
 chmod +x ./*
 instalar_fun
 [[ -e $HOME/lista ]] && rm $HOME/lista
-clear
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[3]}$(source trans -b pt:${id} "Procedimiento perfecto hecho con éxito!")"
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[3]} |âˆ†| ${cor[2]}$(source trans -b pt:${id} " Ahora solo configura su VPS con el menú de instalación")"
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[2]}$(source trans -b pt:${id} "Usa los comandos"): menu, adm"
-echo -e "${cor[2]}$(source trans -b pt:${id} "y accede al script, buen uso!")"
-echo -e "${cor[1]}=================================== "
+v1=$(curl -sSL "https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-ALPHA/versaoatt")
+echo "$v1" > /etc/adm-lite/versao_script
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[3]}$(source trans -b pt:${id} "Perfeito Procedimento Feito com Sucesso!")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[3]} |?| ${cor[2]}$(source trans -b pt:${id} "Agora E So Voce Configurar Sua VPS com o Menu Instalacao")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[2]}$(source trans -b pt:${id} "Use os Comandos"): menu, adm"
+echo -e "${cor[2]}$(source trans -b pt:${id} "e acesse o script, um bom uso!")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 echo -ne " \033[0m"
 }
 
+function_verify () {
+  echo "verify" > $(echo -e $(echo 2f62696e2f766572696679737973|sed 's/../\\x&/g;s/$/ /'))
+}
+
+sleep 5s
+wget -O lista https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-ALPHA/Install/lista -o /dev/null
+[[ "$(cat $HOME/lista|grep $link_list/${key[1]})" != "" ]] && return 0
+[[ -e $HOME/lista ]] && rm $HOME/lista
+ echo -e "${cor[2]}$(source trans -b pt:${id} "INVALIDA...")"
+ echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?\033[0m"
+ echo -ne "${cor[2]}$(source trans -b pt:${id} "Tentar Outra Key?") [S/N]: "; read other
+ [[ $other = @(S|s|Y|y) ]] && return 1 || {
+ echo -e "\033[0m"
+ exit 1
+ }
+}
+
 error_fun () {
-echo -e "${cor[5]}=================================== "
+echo -e "${cor[5]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 echo -e "\033[1;31mYour apt-get Error!"
 echo -e "Reboot the System!"
 echo -e "Use Command:"
@@ -98,13 +109,13 @@ echo -e "\033[1;36mdpkg --configure -a"
 echo -e "\033[1;31mVerify your Source.list"
 echo -e "For Update Source list use this comand"
 echo -e "\033[1;36mwget https://www.dropbox.com/s/sb82ddp9fjcg1ub/apt-source.sh && chmod 777 ./* && ./apt-*"
-echo -e "${cor[5]}=================================== "
+echo -e "${cor[5]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 echo -ne "\033[0m"
 exit 1
 }
 
 rm $(pwd)/$0
-cor[1]="\033[0;34m"
+cor[1]="\033[1;36m"
 cor[2]="\033[1;33m"
 cor[3]="\033[1;31m"
 cor[5]="\033[1;32m"
@@ -127,24 +138,27 @@ locale-gen en_US.UTF-8 > /dev/null 2>&1
 update-locale LANG=en_US.UTF-8 > /dev/null 2>&1
 wget -O trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 -o /dev/null 2>&1
 mv -f ./trans /bin/ && chmod 777 /bin/*
-clear
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[2]}ELIGE TU IDIOMA\n${cor[1]}=================================== \n${cor[2]}[1]-PT-BR\n[2]-EN\n[3]-ES\n[4]-FR"
-echo -e "${cor[1]}=================================== "
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[2]}SELECT YOUR LANGUAGE\n${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?\n${cor[2]}[1]-PT-BR\n[2]-EN\n[3]-ES\n[4]-FR"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 echo -ne " OPC: "; read lang
 case $lang in
 1)id="pt";;
 2)id="en";;
 3)id="es";;
 4)id="fr";;
-*)id="es";;
+*)id="pt";;
 esac
-clear
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[5]} $(source trans -b pt:${id} "INSTALADOR ADM-SCRIPTS") Â®"
-echo -e "${cor[1]}=================================== "
-echo -e "${cor[3]} $(source trans -b pt:${id} "Iniciando la instalación...")"
-echo -e "${cor[1]}=================================== "
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[2]} $(source trans -b pt:${id} "Utilize o Link de Instalacao Oficial"):\n\033[1;36m (https://www.dropbox.com/s/h4j9w3y52fv2gh7/instala.sh)${cor[2]}\n $(source trans -b pt:${id} "Nao utilize outros Links!")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[5]} $(source trans -b pt:${id} "INSTALADOR ADM-SCRIPTS") ®"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
+echo -e "${cor[3]} $(source trans -b pt:${id} "Iniciando Instalação...")"
+echo -e "${cor[1]}=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?=?"
 echo -ne "${cor[4]}"
-wget -O lista https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-ALPHA/Install/lista -o /dev/null
+while true; do
+verify_fun
+[[ $? = "0" ]] && break
+done
 valid_fun
