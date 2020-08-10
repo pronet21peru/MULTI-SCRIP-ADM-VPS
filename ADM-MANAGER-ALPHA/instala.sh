1bb5924a19c2b185b29e4c839d4a99a28b73a0be
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-08-09 21:15:59 -0400

    Update Script

--- ADM-MANAGER-ALPHA/instala.sh
+++ ADM-MANAGER-ALPHA/instala.sh
@@ -81,7 +81,7 @@ echo "$v1" > /etc/adm-lite/versao_script
 echo -e "${cor[1]}=================================== "
 echo -e "${cor[3]}Procedimiento perfecto realizado con Éxito!"
 echo -e "${cor[1]}=================================== "
-echo -e "${cor[3]} |âˆ†| ${cor[2]}Ahora solo configura su VPS con el men� de instalación"
+echo -e "${cor[3]} |âˆ†| ${cor[2]}Ahora solo configura su VPS con el menú de instalación"
 echo -e "${cor[1]}=================================== "
 echo -e "${cor[2]}Utilice los comandos: menú, adm"
 echo -e "${cor[2]}y accede al script, buen uso!"
@@ -132,15 +132,11 @@ update-locale LANG=en_US.UTF-8 > /dev/null 2>&1
 wget -O trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 -o /dev/null 2>&1
 mv -f ./trans /bin/ && chmod 777 /bin/*
 echo -e "${cor[1]}=================================== "
-echo -e "${cor[2]}SELECT YOUR LANGUAGE\n${cor[1]}=================================== \n${cor[2]}[1]-PT-BR\n[2]-EN\n[3]-ES\n[4]-FR"
+
+echo -ne "${cor[2]} ADM-SCRIPTS Â® [N/S]: \033[1;37m"; read x
+[[ $x = @(n|N) ]] && exit
+
 echo -e "${cor[1]}=================================== "
-echo -ne " OPC: "; read lang
-case $lang in
-1)id="pt";;
-2)id="en";;
-3)id="es";;
-4)id="fr";;
-*)id="pt";;
 esac
 echo -e "${cor[1]}=================================== "
 echo -e "${cor[2]} Utilice el enlace de instalación oficial:\n\033[1;36m (https://www.dropbox.com/s/h4j9w3y52fv2gh7/instala.sh)${cor[2]}\n No utilice otros enlaces!"
